83eb6c9b92a6cf4098552302f3e05deae4f21bf5
jorgerleonn <jorgeleonestudios@gmail.com>
2026-09-22 12:33:06 +0200

    tarea: revisar código...

--- Prácticas de Misiles/Practica1_MS.m
+++ Prácticas de Misiles/Practica1_MS.m
@@ -8,32 +8,55 @@
 % y = sin(v);
 % plot(v, y);
 
-%% ---------------------- Código ode45 Clase --------------------
+%% ---------------------- Código ode45 Clase 1 - Movimiento 1D de un misil --------------------
 
 params.g = 9.81; % gravedad
 params.I_sp = 250; % impulso específico
 params.m0 = 1000; % masa inicial del misil
 params.eps = 0.9; % eficiencia del motor
-params.tb = 25; % tiempo de quemado del motor
+params.tb = 5; % tiempo de quemado del motor
 max_step = 0.1; % paso máximo de integración
-tspan = 0:max_step:50; % tiempo de simulación
+tspan = 0:max_step:10000; % tiempo de simulación
+params.Rt = 6371e6; % radio de la tierra
 
 options = odeset('MaxStep',max_step,'RelTol',1e-4,'AbsTol',1e-4, 'Events',@(t,y) event_function(t,y,params));
 
-m = ode45(@(t,y) misil_1D(t,y,params), [0 tspan], [0; 0], options);
+m = ode45(@(t,y)misil_1D(t,y,params), [0 tspan], [0; 0], options);
 
-function dY_dt = misil_1D(t, Y, params) % lanzamiento vertical y sin resistencia del aire
-    dY_dt(1,1) = params.g*params.I_sp*params.eps/params.tb/(1-params.eps*t/params.tb)*(t<params.tb) - params.g; % velocidad
+function g = gravity(y, params)
+    g = params.g * params.Rt^2 / (y + params.Rt)^2;
+end
+
+function dY_dt = misil_1D(t, Y, p) % lanzamiento vertical y sin resistencia del aire
+    dY_dt(1,1) = p.g*p.I_sp*p.eps/p.tb/(1-p.eps*t/p.tb)*(t<p.tb) - gravity(y(2), p); % velocidad
     dY_dt(2,1) = Y(1); % posición
 end
 
-function [position, isterminal, direction] = event_function(t, y, params)
-    position = [y(2)]; % evento cuando la posición es cero (misil toca el suelo)
-    isterminal = [1; 0]; % detener la integración
-    direction = [-1; 0]; % solo detectar cuando la posición disminuye
+function [position, isterminal, direction] = event_function(t, y, params) % evento para detectar cuando el misil toca el suelo
+    position = [y(2), params.tb-t, y(1)]; % evento cuando la posición es cero (misil toca el suelo)
+    isterminal = [1, 0, 0]; % detener la integración
+    direction = [-1, -1, -1]; % solo detectar cuando la posición disminuye
 end
 
 t = m.x; % tiempo de simulación
 y = m.y; % resultados de la simulación
-z = y(1,:); % posición
-v = y(2,:); % velocidad
+v = y(1,:); % posición
+z = y(2,:); % velocidad
+vb = m.ye(1,1); % velocidad al final del quemado
+
+figure(1);
+subplot(2,1,1);
+plot(t, v);
+% punto de fin de combustión
+plot(t, vb, 'ro', 'MarkerSize', 2, 'LineWidth', 2); % marcador rojo para la velocidad al final del quemado
+ylabel('Velocidad (m/s)');
+xlabel('Tiempo (s)');
+title('Velocidad del misil en función del tiempo');
+
+
+figure(2);
+subplot(2,1,1);
+plot(t, z);
+ylabel('Posición (m)');
+xlabel('Tiempo (s)');
+title('Posición del misil en función del tiempo');
